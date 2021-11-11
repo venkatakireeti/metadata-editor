15708bfb982d924198cef6786078d312b1b3e515
venkatakireeti <89148698+venkatakireeti@users.noreply.github.com>
2021-11-11 19:08:17 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: "mongodb://root:123456@localhost:27017/metadata?authSource=admin&replicaSet=rs0&readPreference=primary&appname=MongoDB%20Compass&retryWrites=false&directConnection=true&ssl=false"
+  url: "mongodb://root:123456@18.217.55.36:27017/metadata?authSource=admin&replicaSet=rs0&readPreference=primary&appname=MongoDB%20Compass&retryWrites=false&directConnection=true&ssl=false"
 };
